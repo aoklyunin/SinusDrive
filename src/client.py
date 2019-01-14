ad19b511735e65f5953ddb6fe4891a3d0bf69bf8
alex <aoklyunin@gmail.com>
2019-01-14 23:18:10 +0300

    gui ready

--- src/client.py
+++ src/client.py
@@ -9,36 +9,73 @@ loadedFilename = ""
 
 window = Tk()
 window.title("Sinus Drive App")
-window.geometry('150x80')
+window.geometry('300x150')
 
 lbl = Label(window, text="Press load csv file")
 lbl.grid(column=0, columnspan=2, row=0)
+
+var = IntVar()
+
+c = Checkbutton(window, text="Range", variable=var)
+c.grid(column=0, columnspan=2, row=2)
+
+
+
+sl = Label(window, text="Start")
+sl.grid(column=0, row=3)
+
+fl = Label(window, text="Finish")
+fl.grid(column=1, row=3)
+
+start = Entry(window)
+start.grid(column=0, row=4)
+
+start.delete(0, END)
+start.insert(0, "0")
+
+end = Entry(window)
+end.grid(column=1, row=4)
+
+end.delete(0, END)
+end.insert(0, "100000")
+
 
 def load():
     global resSQD
     global resMA
     global loadedFilename
-
+    global var
 
     filename = filedialog.askopenfilename(initialdir=".", title="Select file",
-                                            filetypes=(("csv", "*.csv"), ("all files", "*.*")))
+                                          filetypes=(("csv", "*.csv"), ("all files", "*.*")))
 
     lbl.configure(text="Calculation in work... wait")
     lbl.update()
     loadedFilename = filename.split('/')[-1]
 
-    resSQD = getSQD(filename)
-    resMA = getMeanAmp(filename)
+    if var:
+        print(int(start.get()), int(end.get()))
+        resMA = getMeanAmp(filename, int(start.get()), int(end.get()))
+        resSQD = getSQD(filename, int(start.get()), int(end.get()))
+    else:
+        resMA = getMeanAmp(filename)
+        resSQD = getSQD(filename)
+
+    print("calculations ready")
     lbl.configure(text="Calculation ready")
     lbl.update()
+
 
 def save():
     savefolder = filedialog.askdirectory(initialdir=".", title="Select folder")
     print(savefolder)
-    wrtePlot(resMA[0], resMA[1], resMA[2], savefolder +"/mean_amplitude_"+ loadedFilename.split('.')[0] + '.png', "mean_amplitude_" + loadedFilename.split('.')[0])
-    wrtePlot(resSQD[0], resSQD[1], resSQD[2], savefolder +"/sqd_"+ loadedFilename.split('.')[0] + '.png', "sqd_" + loadedFilename.split('.')[0])
+    wrtePlot(resMA[0], resMA[1], resMA[2], savefolder + "/mean_amplitude_" + loadedFilename.split('.')[0] + '.png',
+             "mean_amplitude_" + loadedFilename.split('.')[0])
+    wrtePlot(resSQD[0], resSQD[1], resSQD[2], savefolder + "/sqd_" + loadedFilename.split('.')[0] + '.png',
+             "sqd_" + loadedFilename.split('.')[0])
 
     lbl.configure(text="images are saved")
+
 
 btn = Button(window, text="Load", command=load)
 btn.grid(column=0, row=1)
--- src/test.py
+++ src/test.py
@@ -2,13 +2,13 @@ from src.driveSinus import getSQD, wrtePlot, getMeanAmp
 
 
 def getResult(source):
-    res = getSQD('../drive_data/' + source + '.csv', 20000, 40000)
+    res = getSQD('../drive_data/' + source + '.csv', 0, 20000)
     wrtePlot(res[0], res[1], res[2], '../imgs/sqd_' + source + '.png', "sqd_" + source)
-    res = getMeanAmp('../drive_data/' + source + '.csv', 20000, 40000)
+    res = getMeanAmp('../drive_data/' + source + '.csv', 0, 20000)
     wrtePlot(res[0], res[1], res[2], '../imgs/mean_amplitude_' + source + '.png', "mean_amplitude_" + source)
 
 
-getResult("4-1 CSV")
-# getResult("4-2 CSV")
-# getResult("5 2 csv")
+#getResult("4-1 CSV")
+#getResult("4-2 CSV")
+getResult("5 2 csv")
 # getResult("6")
