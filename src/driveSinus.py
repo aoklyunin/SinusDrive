31d1ebfd4dc9ae9500ea6b97d28e16adfc7f31bf
alex <aoklyunin@gmail.com>
2019-01-14 17:02:29 +0300

    client start

--- src/driveSinus.py
+++ src/driveSinus.py
@@ -13,10 +13,25 @@ def getLst(lst, coeffs):
     return newLst
 
 
+def wrtePlot(t, sinusLst, newSinusLst, path, caption):
+    fig, axs = plt.subplots(2, 1)
+    axs[0].plot(t, sinusLst, t, newSinusLst)
+    axs[0].set_xlabel('time')
+    axs[0].set_ylabel('mes and approx')
+    axs[0].grid(True)
+
+    axs[1].plot(t, newSinusLst)
+    axs[1].set_xlabel('time')
+    axs[1].set_ylabel('approx')
+    axs[1].grid(True)
+
+    fig.suptitle(caption, fontsize=6)
+    fig.tight_layout()
+    plt.savefig(path)
+
+
 def findSinusCoeffs(lst):
-    # find amplitude
-    amp0 = max(lst)
-    # find frequency
+    prevL = lst[0]
 
     flgDrop = False
     zerosLst = []
@@ -27,6 +42,9 @@ def findSinusCoeffs(lst):
                 flgDrop = True
         else:
             flgDrop = False
+
+    # find phi0
+    phi0 = zerosLst[0] - np.pi / 2
 
     periodsLst = []
     for i in range(len(zerosLst) - 1):
@@ -40,10 +58,66 @@ def findSinusCoeffs(lst):
     meanPeriod = periodSum / len(periodsLst)
     omega0 = 1 / meanPeriod
 
-    # find phi0
-    phi0 = -np.pi
+    amps = []
 
-    phi0 = zerosLst[0] - np.pi / 2
+    # find amplitude
+    amp0 = max(lst)
+    print("amp: ", amp0)
+    flgAddAmp = False
+
+    meanAmps = []
+
+
+
+    for l in lst:
+        if (l > 0 and amp0 - l < 0.2):
+            state = "positive"
+           # print("make pos")
+            break
+        if (l < 0 and -amp0 - l > -0.2):
+            state = "negative"
+           # print("make neg")
+            break
+
+    for l in lst:
+        if (l > 0 and amp0 - l < 0.2):
+            if state == "negative" and len(amps) > 0:
+                meanAmps.append(np.mean(amps))
+              #  print("loop mean ", np.mean(amps))
+                amps.clear()
+            if l > prevL:
+                flgAddAmp = True
+            else:
+                if flgAddAmp:
+                    amps.append(l)
+                flgAddAmp = False
+            state = "positive"
+
+        if (l < 0 and -amp0 - l > -0.2):
+            if state == "positive" and len(amps) > 0:
+                meanAmps.append(np.mean(amps))
+             #   print("loop mean ", np.mean(amps))
+                amps.clear()
+
+          #  print("negative")
+            if l < prevL:
+                flgAddAmp = True
+            else:
+                if flgAddAmp:
+                    amps.append(-l)
+                flgAddAmp = False
+            state = "negative"
+
+        prevL = l
+
+    # for amp in amps:
+    #     print(amp)
+
+    amp0 = np.mean(meanAmps)
+    # find frequency
+    print("mamp ", amp0)
+
+
 
     return [amp0, omega0, phi0]
 
@@ -56,7 +130,7 @@ def getDistance(lst, newLst):
     return sum
 
 
-def getSQDPic(sourse, target, caption):
+def readLstFromFile(sourse):
     with open(sourse, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         lst = []
@@ -65,9 +139,12 @@ def getSQDPic(sourse, target, caption):
                 lst.append(float(row[0]))
             except:
                 pass
+    return lst
 
-    # sinusLst = lst[:10000]
-    sinusLst = lst
+
+def getSQD(sourse):
+    sinusLst = readLstFromFile(sourse)
+    # sinusLst = sinusLst[:10000]
 
     coeffs = findSinusCoeffs(sinusLst)
     # newSinusLst = getLst(lst, coeffs)
@@ -75,66 +152,26 @@ def getSQDPic(sourse, target, caption):
 
     t = np.arange(0, len(sinusLst))
 
-    fig, axs = plt.subplots(2, 1)
-    axs[0].plot(t, sinusLst, t, newSinusLst)
-    axs[0].set_xlabel('time')
-    axs[0].set_ylabel('mes and approx')
-    axs[0].grid(True)
-
-    axs[1].plot(t, newSinusLst)
-    axs[1].set_xlabel('time')
-    axs[1].set_ylabel('approx')
-    axs[1].grid(True)
-    fig.suptitle(caption, fontsize=6)
-    fig.tight_layout()
-    plt.savefig(target)
+    return [t, sinusLst, newSinusLst]
 
 
-#   plt.show()
-
-
-def getMeanAmpPic(sourse, target, caption):
-    with open(sourse, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        lst = []
-        for row in spamreader:
-            try:
-                lst.append(float(row[0]))
-            except:
-                pass
-
-    sinusLst = lst[:10000]
-    #sinusLst = lst
+def getMeanAmp(sourse):
+    sinusLst = readLstFromFile(sourse)
+    # sinusLst = sinusLst[:10000]
 
     coeffs = findSinusCoeffs(sinusLst)
     newSinusLst = getLst(sinusLst, coeffs)
 
     t = np.arange(0, len(sinusLst))
 
-    fig, axs = plt.subplots(2, 1)
-    axs[0].plot(t, sinusLst, t, newSinusLst)
-    axs[0].set_xlabel('time')
-    axs[0].set_ylabel('mes and approx')
-    axs[0].grid(True)
-
-    axs[1].plot(t, newSinusLst)
-    axs[1].set_xlabel('time')
-    axs[1].set_ylabel('approx')
-    axs[1].grid(True)
-
-    fig.suptitle(caption, fontsize=6)
-    fig.tight_layout()
-    plt.savefig(target)
-
-
-# plt.show()
+    return [t, sinusLst, newSinusLst]
 
 
 def preciseFind(lst, coeffs):
     preciseCoeffs = []
     minDist = -1
     for amp in np.arange(coeffs[0] * 0.8, coeffs[0] * 1.2, coeffs[0] * 0.05):
-        #print("loop amp: ",amp)
+        # print("loop amp: ",amp)
         for omega in np.arange(coeffs[1] * 0.8, coeffs[1] * 1.2, coeffs[1] * 0.05):
             for phi in np.arange(coeffs[2] - np.pi / 40, coeffs[2] + np.pi / 40, np.pi / 80):
                 newCoeffs = [amp, omega, phi]
@@ -155,14 +192,3 @@ def preciseFind(lst, coeffs):
     print("preciseCoeffs: ", preciseCoeffs)
     return getLst(lst, preciseCoeffs)
 
-
-def getResult(source):
-    getSQDPic('../drive_data/' + source + '.csv', '../imgs/sqd_' + source + '.png', "sqd_" + source)
-    getMeanAmpPic('../drive_data/' + source + '.csv', '../imgs/mean_amplitude_' + source + '.png',
-                  "mean_amplitude_" + source)
-
-
-# getResult("4-1 CSV")
-# getResult("4-2 CSV")
-# getResult("5 2 csv")
-getResult("6")
---
+++ src/test.py
@@ -0,0 +1,14 @@
+from src.driveSinus import getSQD, wrtePlot, getMeanAmp
+
+
+def getResult(source):
+    res = getSQD('../drive_data/' + source + '.csv')
+    wrtePlot(res[0], res[1], res[2], '../imgs/sqd_' + source + '.png', "sqd_" + source)
+    res = getMeanAmp('../drive_data/' + source + '.csv')
+    wrtePlot(res[0], res[1], res[2], '../imgs/mean_amplitude_' + source + '.png', "mean_amplitude_" + source)
+
+
+getResult("4-1 CSV")
+getResult("4-2 CSV")
+getResult("5 2 csv")
+getResult("6")
