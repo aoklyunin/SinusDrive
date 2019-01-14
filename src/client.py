ca8b39493655d9b22a133aa9a08e17fe382fc8c9
alex <aoklyunin@gmail.com>
2019-01-14 17:29:45 +0300

    application ready

--- src/client.py
+++ src/client.py
@@ -1,98 +1,49 @@
-#!/usr/bin/python
-# -*- coding: <<encoding>> -*-
-# -------------------------------------------------------------------------------
-#   <<project>>
-#
-# -------------------------------------------------------------------------------
+from tkinter import *
+from tkinter import filedialog
 
-import wxversion
+from src.driveSinus import getSQD, getMeanAmp, wrtePlot
 
-wxversion.select("2.8")
-import wx, wx.html
-import sys
+resSQD = []
+resMA = []
+loadedFilename = ""
 
-aboutText = """<p>Sorry, there is no information about this program. It is
-running on version %(wxpy)s of <b>wxPython</b> and %(python)s of <b>Python</b>.
-See <a href="http://wiki.wxpython.org">wxPython Wiki</a></p>"""
+window = Tk()
+window.title("Sinus Drive App")
+window.geometry('150x80')
 
+lbl = Label(window, text="Press load csv file")
+lbl.grid(column=0, columnspan=2, row=0)
 
-class HtmlWindow(wx.html.HtmlWindow):
-    def __init__(self, parent, id, size=(600, 400)):
-        wx.html.HtmlWindow.__init__(self, parent, id, size=size)
-        if "gtk2" in wx.PlatformInfo:
-            self.SetStandardFonts()
-
-    def OnLinkClicked(self, link):
-        wx.LaunchDefaultBrowser(link.GetHref())
+def load():
+    global resSQD
+    global resMA
+    global loadedFilename
 
 
-class AboutBox(wx.Dialog):
-    def __init__(self):
-        wx.Dialog.__init__(self, None, -1, "About <<project>>",
-                           style=wx.DEFAULT_DIALOG_STYLE | wx.THICK_FRAME | wx.RESIZE_BORDER |
-                                 wx.TAB_TRAVERSAL)
-        hwin = HtmlWindow(self, -1, size=(400, 200))
-        vers = {}
-        vers["python"] = sys.version.split()[0]
-        vers["wxpy"] = wx.VERSION_STRING
-        hwin.SetPage(aboutText % vers)
-        btn = hwin.FindWindowById(wx.ID_OK)
-        irep = hwin.GetInternalRepresentation()
-        hwin.SetSize((irep.GetWidth() + 25, irep.GetHeight() + 10))
-        self.SetClientSize(hwin.GetSize())
-        self.CentreOnParent(wx.BOTH)
-        self.SetFocus()
+    filename = filedialog.askopenfilename(initialdir=".", title="Select file",
+                                            filetypes=(("csv", "*.csv"), ("all files", "*.*")))
 
+    lbl.configure(text="Calculation in work... wait")
+    lbl.update()
+    loadedFilename = filename.split('/')[-1]
 
-class Frame(wx.Frame):
-    def __init__(self, title):
-        wx.Frame.__init__(self, None, title=title, pos=(150, 150), size=(350, 200))
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
+    resSQD = getSQD(filename)
+    resMA = getMeanAmp(filename)
+    lbl.configure(text="Calculation ready")
+    lbl.update()
 
-        menuBar = wx.MenuBar()
-        menu = wx.Menu()
-        m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
-        self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
-        menuBar.Append(menu, "&File")
-        menu = wx.Menu()
-        m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
-        self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
-        menuBar.Append(menu, "&Help")
-        self.SetMenuBar(menuBar)
+def save():
+    savefolder = filedialog.askdirectory(initialdir=".", title="Select folder")
+    print(savefolder)
+    wrtePlot(resMA[0], resMA[1], resMA[2], savefolder +"/mean_amplitude_"+ loadedFilename.split('.')[0] + '.png', "mean_amplitude_" + loadedFilename.split('.')[0])
+    wrtePlot(resSQD[0], resSQD[1], resSQD[2], savefolder +"/sqd_"+ loadedFilename.split('.')[0] + '.png', "sqd_" + loadedFilename.split('.')[0])
 
-        self.statusbar = self.CreateStatusBar()
+    lbl.configure(text="images are saved")
 
-        panel = wx.Panel(self)
-        box = wx.BoxSizer(wx.VERTICAL)
+btn = Button(window, text="Load", command=load)
+btn.grid(column=0, row=1)
 
-        m_text = wx.StaticText(panel, -1, "Hello World!")
-        m_text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
-        m_text.SetSize(m_text.GetBestSize())
-        box.Add(m_text, 0, wx.ALL, 10)
+btn = Button(window, text="Save", command=save)
+btn.grid(column=1, row=1)
 
-        m_close = wx.Button(panel, wx.ID_CLOSE, "Close")
-        m_close.Bind(wx.EVT_BUTTON, self.OnClose)
-        box.Add(m_close, 0, wx.ALL, 10)
-
-        panel.SetSizer(box)
-        panel.Layout()
-
-    def OnClose(self, event):
-        dlg = wx.MessageDialog(self,
-                               "Do you really want to close this application?",
-                               "Confirm Exit", wx.OK | wx.CANCEL | wx.ICON_QUESTION)
-        result = dlg.ShowModal()
-        dlg.Destroy()
-        if result == wx.ID_OK:
-            self.Destroy()
-
-    def OnAbout(self, event):
-        dlg = AboutBox()
-        dlg.ShowModal()
-        dlg.Destroy()
-
-
-app = wx.App(redirect=True)  # Error messages go to popup window
-top = Frame("<<project>>")
-top.Show()
-app.MainLoop()
+window.mainloop()
